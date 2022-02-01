859cf50c5974e911207367cb69cc5c09c03ef72c
Lodewijk Brand <lodewijk.brand@gmail.com>
2022-02-01 13:47:09 -0500

    feat: add docker install script

--- .bin/docker-fix-permissions.sh
+++ .bin/docker-fix-permissions.sh
@@ -1,4 +1,3 @@
 systemctl start docker
 sudo usermod -aG docker $USER
 newgrp docker
-docker run hello-world
---
+++ .bin/docker-install.sh
@@ -0,0 +1,3 @@
+# Quickly install Docker from CLI
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
